9492c440d7050b0ebd5cd8e8bcba85b84cef5c23
Nackwon-Do <ahwk321@gmail.com>
2018-05-21 21:27:30 +0900

    BOJ_Step02

--- BOJ_Step02/BOJ_2839.py
+++ BOJ_Step02/BOJ_2839.py
@@ -1,10 +1,15 @@
 sugar = int(input())
 count = 0
+
 while sugar % 5 != 0:
     if sugar < 0:
         count = -1
         break
-    sugar -= 3
-    count += 1
-count += sugar // 5
+    else:
+        sugar -= 3
+        count += 1
+
+if sugar % 5 == 0:
+    count += sugar // 5
 print(count)
+
